2a62a1e0c037f6fa57711b2f273e20878cb8555c
riswan ardiansah <riswanardiansah@gmail.com>
2024-01-17 15:10:45 +0700

    feat(dashboard): integrate dashboard

--- src/app/report/report.service.ts
+++ src/app/report/report.service.ts
@@ -25,8 +25,8 @@ export const getDailyReportService = async ( date: Dayjs, typeCash: string = E_C
   const rawYesterdayCashFlow = cashFlow.filter( cf => dayjs( cf?.created_at ).format( DateFormat ) === date.subtract( 1, 'day' ).format( DateFormat ) )
 
   const todayCashFlow: CashFlowResponseItem[] = rawTodayCashFlow.map( cf => {
-    const payDebtAmount = cf.transaction_id ? cf.transaction.pay_debt_amount : 0
-    const customerName = cf.customer_id ? cf.customer.name : cf.transaction.customer?.name
+    const payDebtAmount = cf.transaction ? cf.transaction_id ? cf.transaction.pay_debt_amount : 0 : 0
+    const customerName = cf.customer ? cf.customer_id ? cf.customer.name : cf.transaction.customer?.name : ''
     return {
       id                : cf.id,
       note              : cf.note,
--- src/app/statistic/statistic.router.ts
+++ src/app/statistic/statistic.router.ts
@@ -1,12 +1,13 @@
 import {
   Controller, Get, Route, Tags
 } from 'tsoa'
+import { getDashboardStatsService } from './statistic.service'
 
 @Tags( 'Statistic' )
 @Route( '/api/statistic' )
 export class StatisticController extends Controller {
   @Get( '/dashboard' )
   public async getDashboardStats () {
-    
+    return await getDashboardStatsService()
   }
 }
